564b65beeff7c7bcd1ac298db346ac6992064755
Jack Glazer <91999893+jackrobert0220@users.noreply.github.com>
2022-09-24 14:08:49 -0700

    changed favicon

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,27 +6,25 @@ import prVideo from "../assets/PR-Video.mp4";
 
 const Video = () => {
   return (
-    
-      <div className="hero">
-        <video autoPlay loop muted playsInline id="video">
-          <source src={prVideo} type="video/mp4" />
-        </video>
-        <div className="content">
-          <h1>Plastic Rhino</h1>
-          <p>Los Angeles-based Hard Rock Band</p>
+    <div className="hero">
+      <video autoPlay loop muted playsInline id="video">
+        <source src={prVideo} type="video/mp4" />
+      </video>
+      <div className="content">
+        <h1>Plastic Rhino</h1>
+        <p>Los Angeles-based Hard Rock Band</p>
 
-          <Link to="./music" className="btn">
-            Listen
-          </Link>
-          <Link to="./videos" className="btn">
-            Watch
-          </Link>
-          <Link to="./shows" className="btn">
-            Shows
-          </Link>
-        </div>
+        <Link to="./music" className="btn">
+          Listen
+        </Link>
+        <Link to="./videos" className="btn">
+          Watch
+        </Link>
+        <Link to="./shows" className="btn">
+          Shows
+        </Link>
       </div>
-
+    </div>
   );
 };
 
